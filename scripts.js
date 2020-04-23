cea2f204877897ea7d55dc9407e9f7a4e8ffcfa3
OntottVas <kgergely1607@gmail.com>
2020-04-23 16:43:31 +0200

    Created HTML, CSS, JavaScript files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let name = 'OntottVas';
+
+console.log(name + "'s apples");
+console.log('Hello World!');
+
+// This is a JavaScript comment.
+
+/*
+    This is a
+    multiline
+    JavaScript
+    comment.
+*/
